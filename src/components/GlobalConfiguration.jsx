0f34b3b639a0aa23ed19732c255c5e829b482cce
Kartikay1745 <kartikay.sinha@revolvespl.com>
2025-09-05 10:31:08 -0400

    updated code

--- src/components/GlobalConfiguration.jsx
+++ src/components/GlobalConfiguration.jsx
@@ -2405,7 +2405,9 @@ const LaborForm = () => {
             type="button"
             onClick={handleSaveAllSettings}
             className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2.5 px-5 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200"
-          ></button>
+          >
+            Save Setting
+          </button>
         </div>
       </div>
     </div>
